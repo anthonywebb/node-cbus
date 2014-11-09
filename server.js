0e0bb71c2bb20b899fa7e42ae48a9effaa1bc88d
Anthony Webb <anthony.webb@gmail.com>
2014-11-09 02:27:43 -0700

    cleanup

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 var net = require('net');
 var sys = require('sys');
 var carrier = require('carrier');
-
 var config = require('./config');
 
+////////////////////////
 // WEBSERVER SETUP
+////////////////////////
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+app.use('/',express.static(__dirname + '/public'));
+server.listen(config.webserver.port,config.webserver.host);
+console.log('Ready on: http://'+config.webserver.host+':'+config.webserver.port);
+
+app.get('/do', function (req, res) {
+	if (req.query.cmd) {
+		var command = req.query.cmd.trim() + '\r';
+		console.log('remoteCommand : ' + command);
+		control.write(command);
+		res.send({ status: 'ok', executed: req.query.cmd});
+	}
+	else {
+		res.send({ status: 'error', message: 'you must specify a command'},400);
+	}
+	
+});
+
+////////////////////////
+// CGATE CONNECTION
+////////////////////////
 
 // TELNET SESSION TO CONTROL
 var control = net.createConnection(config.cgate.contolport,config.cgate.host);
@@ -30,10 +52,13 @@ carrier.carry(statuses, function(line) {
 
 function pushRealtime(type, message) {
 	console.log(type+' : '+message);
-	io.emit(type, parseMessage(message));
+	// every message, before being sent out needs to be parsed to create a nice object that can be consumed
+  io.emit(type, parseMessage(message));
 }
 
+////////////////////////
 // MESSAGE PROCESSING
+////////////////////////
 function parseMessage(data) {
 	console.log(data);
 
@@ -92,21 +117,3 @@ function parseMessage(data) {
 function processMessage(msg) {
   
 }
-
-// WEB SERVER
-app.use('/',express.static(__dirname + '/public'));
-server.listen(config.webserver.port,config.webserver.host);
-console.log('Ready on: http://'+config.webserver.host+':'+config.webserver.port);
-
-app.get('/do', function (req, res) {
-	if (req.query.cmd) {
-		var command = req.query.cmd.trim() + '\r';
-		console.log('remoteCommand : ' + command);
-		control.write(command);
-		res.send({ status: 'ok', executed: req.query.cmd});
-	}
-	else {
-		res.send({ status: 'error', message: 'you must specify a command'},400);
-	}
-	
-});
