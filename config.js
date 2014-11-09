b5bcead307defad83376c95b7ca8fa2e1670b43d
Anthony Webb <anthony.webb@gmail.com>
2014-11-09 01:54:23 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+    cgate: {
+        host: '192.168.11.40',
+        contolport: 20023,
+        eventport: 20024,
+        statusport: 20025
+    },
+    webserver: {
+        port: 8080,
+        host: '0.0.0.0'
+    }
+};
+
+module.exports = config;
