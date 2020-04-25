a16bd973649a3b9cd00b5f9f0c5af351886a52c9
Anthony Webb <anthony.webb@gmail.com>
2020-04-25 01:14:22 -0600

    Update cgate.js
    Bug fix wrong array element

--- cgate.js
+++ cgate.js
@@ -144,7 +144,7 @@ function parseMessage(data,type) {
 
     // last element of arr2 is the group
     temp = array[2].match(/\d+/g);
-    packet.group = temp[3];
+    packet.group = temp[2];
 
     var parseunit = array[3];
     var parseoid = array[4];
